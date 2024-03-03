1b0bcd9177307635dfd645fcc0348e0a3332eaf2
aldoprogrammer <aldobesma@gmail.com>
2024-03-03 10:54:03 +0700

    update

--- src/dashboard/DashboardLayout.jsx
+++ src/dashboard/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import { Sidebar } from 'flowbite-react'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import SideBar from './Sidebar'
+import SideBar from './SideBar'
 
 const DashboardLayout = () => {
   return (
